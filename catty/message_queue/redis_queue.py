9d0f57d0d76bae1bca479a0df6b614032b474c6f
钟悦辉 <zhongyuehui@tungee.com>
2017-03-08 20:08:20 +0800

    add async_message_queue

--- catty/message_queue/redis_queue.py
+++ catty/message_queue/redis_queue.py
@@ -8,8 +8,10 @@
 import pickle
 import queue
 import time
+from asyncio import queues as async_queue
 
 import redis
+import aioredis
 
 from catty.libs.utils import get_default
 
@@ -132,3 +134,101 @@ class RedisPriorityQueue(BaseQueue):
                         raise
                 else:
                     time.sleep(self.max_timeout)
+
+
+class BaseAsyncQueue(object):
+    # 引用标准库queue中的Exception
+    Empty = async_queue.QueueEmpty
+    Full = async_queue.QueueFull
+
+    def __init__(self, name, loop, host='localhost', port='6379', db=0, password=None, pool_maxsize=10):
+        """
+        :param name:        队列的名字
+        :param loop:        EventLoop
+        :param host:        redis所在主机
+        :param port:        端口号
+        :param db:          db
+        :param password:    redis密码
+        :return:
+        """
+        self.name = name
+        self.loop = loop
+
+        self.host = host
+        self.port = port
+        self.db = db
+        self.pw = password
+
+        self.redis_conn = None
+        self.redis_pool = None
+
+        self.pool_maxsize = pool_maxsize
+
+    async def conn_pool(self):
+        self.redis_pool = await aioredis.create_pool(
+            (self.host, self.port), db=self.db, loop=self.loop, maxsize=self.pool_maxsize
+        )
+        return self.redis_pool
+
+    async def conn(self):
+        self.redis_conn = await aioredis.create_redis(
+            (self.host, self.port), db=self.db, loop=self.loop
+        )
+
+    async def put(self, item):
+        raise NotImplementedError
+
+    async def get(self):
+        raise NotImplementedError
+
+    async def clear(self):
+        self.redis_conn.delete(self.name)
+
+
+class AsyncRedisPriorityQueue(BaseAsyncQueue):
+    def __init__(self, name, loop, host='localhost', port=6379, db=0,
+                 queue_maxsize=100, password=None, pool_maxsize=10):
+        """
+        :param queue_maxsize:     队列的最大上限
+        :return:
+        """
+        super(AsyncRedisPriorityQueue, self).__init__(name, loop, host, port, db, password, pool_maxsize)
+        self.queue_maxsize = queue_maxsize
+        self.last_qsize = 0
+
+    async def qsize(self):
+        self.last_qsize =await self.redis_conn.zcard(self.name)
+        return self.last_qsize
+
+    async def empty(self):
+        qsize = await self.qsize()
+        if qsize == 0:
+            return True
+        else:
+            return False
+
+    async def full(self):
+        if self.queue_maxsize and await self.qsize() >= self.queue_maxsize:
+            return True
+        else:
+            return False
+
+    async def get(self):
+        pipe = self.redis_conn.pipeline()
+        pipe.zrange(self.name, 0, 0)
+        pipe.zremrangebyrank(self.name, 0, 0)
+        result, count = await pipe.execute()
+        if not result:
+            raise self.Empty
+
+        return pickle.loads(result[0])
+
+    async def put(self, item):
+        is_full = await self.full()
+        if is_full:
+            raise self.Full
+
+        priority = -get_default(item, 'priority', 0)
+        await self.redis_conn.zadd(self.name, priority, pickle.dumps(item))
+        return True
+

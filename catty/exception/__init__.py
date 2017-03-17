b97734b6a32f50f9867907a2738229017c38181b
wax8280 <wax8280@163.com>
2017-03-17 18:34:56 +0800

    add queue exception

--- catty/exception/__init__.py
+++ catty/exception/__init__.py
@@ -4,7 +4,16 @@
 # Author: Vincent<vincent8280@outlook.com>
 #         http://blog.vincentzhong.cn
 # Created on 2017/2/26 18:37
+import asyncio
+import queue
+
 
 class Retry_current_task(Exception):
     def __str__(self):
         return "Retry current task."
+
+
+AsyncQueueEmpty = asyncio.queues.QueueEmpty
+AsyncQueueFull = asyncio.queues.QueueFull
+QueueEmpty = queue.Empty
+QueueFull = queue.Full

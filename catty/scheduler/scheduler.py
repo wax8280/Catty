298fdff20774acc61e742822a715333f6a6cafb5
wax8280 <wax8280@163.com>
2017-03-27 22:32:13 +0800

    将scheduler队列改为异步

--- catty/scheduler/scheduler.py
+++ catty/scheduler/scheduler.py
@@ -5,10 +5,15 @@
 #         http://blog.vincentzhong.cn
 # Created on 2017/2/24 13:14
 
+import threading
 import time
 import traceback
+from queue import PriorityQueue
 
-from catty.message_queue.redis_queue import RedisPriorityQueue
+from catty.config import CONFIG
+from catty.exception import *
+from catty.libs.handle_module import SpiderModuleHandle
+from catty.message_queue.redis_queue import AsyncRedisPriorityQueue
 from catty.scheduler.tasker import Tasker
 
 NOTSTART = 0
@@ -16,116 +21,173 @@ STARTED = 1
 READY_TO_START = 2
 STOP = 3
 
+LOAD_QUEUE_SLEEP = 1
+SELECTOR_SLEEP = 3
+
 
 class Scheduler(object):
     LOAD_SPIDER_INTERVAL = 1
-    # schema
-    # [
-    #   {
-    #       'spider': spdier_ins,
-    #       'name': spider_name
-    #       'status': int,   # 0 not_start 1 start
-    #   }
-    # ]
-    SPIDERS = []
-    COUNT_PRE_LOOP = 30
 
     def __init__(self,
-                 scheduler_downloader_queue: RedisPriorityQueue,
-                 parser_scheduler_queue: RedisPriorityQueue):
+                 scheduler_downloader_queue: AsyncRedisPriorityQueue,
+                 parser_scheduler_queue: AsyncRedisPriorityQueue,
+                 loop):
         self.scheduler_downloader_queue = scheduler_downloader_queue
         self.parser_scheduler_queue = parser_scheduler_queue
 
         self._stop = False
-        self._count_pre_loop = self.COUNT_PRE_LOOP
-        self._task_from_parser = []
+        self.task_from_parser = PriorityQueue()
         # _task_to_downloader:the task waiting for exetime
-        self._task_to_downloader = []
+        self.task_to_downloader = PriorityQueue()
         # _selected_task:the task already after exetime
-        self._selected_task = []
+        self.selected_task = PriorityQueue()
 
         self.tasker = Tasker()
-        # [
-        #   {'spider_name':string,'spider_class':class}
-        # ]
-        self.spdier_loaded = []
 
-        self.spider_insed = set()
         self.spider_stoped = set()
         self.spider_started = set()
 
-    def load_spider(self):
-        """load script from db"""
-        pass
+        # schema
+        # {
+        #   'spider_name':{status,}
+        # }
+        self.loaded_spider = {}
 
-    def _instantiate_spider(self, spider_class, spider_name, status=0):
-        """instantiate the spider"""
-        self.SPIDERS.append({
-            'spider': spider_class(),
-            'name': spider_name,
-            'status': status
-        })
+        self.loop = loop
+        self.lock = threading.Lock()
+
+        self.spider_module_handle = SpiderModuleHandle(CONFIG['SPIDER_PATH'])
+        # self.spider_module_handle.to_instance_spider()
 
     def instantiate_spider(self):
         """instantiate all spider which had loaded and not instantiated"""
-        for spider in self.spdier_loaded:
-            if spider['spider_name'] not in self.spider_insed:
-                self._instantiate_spider(
-                    spider['spider_class'],
-                    spider['spider_name'],
-                )
+        self.spider_module_handle.to_instance_spider()
+        for spider_name in self.spider_module_handle.namespace.keys():
+            try:
+                spider_name = getattr(self.spider_module_handle.namespace[spider_name][1], 'Spider').name
+            except AttributeError:
+                continue
+            self.loaded_spider.update({spider_name: {'status': READY_TO_START}})
 
-    def _select_task(self):
+    # ---------------------Thread async queue-----------------
+    def select_task(self):
         """base on the exetime select the task to push"""
-        for each_task in self._task_to_downloader:
-            if time.time() - each_task['scheduler']['exetime'] > 0:
-                self._selected_task.append(each_task)
-
-    def _get_item_from_parser_scheduler_queue(self):
-        """get item from parser-scheduler queue"""
-        # TODO catch the exception
-        return self.parser_scheduler_queue.get_nowait()
-
-    def _push_task_to_scheduler_parser_queue(self):
-        """push task to scheduler-downloader queue"""
+        all_task = []
+        # TODO 暂时没有找到好的遍历队列的方法
+        # 全部出队
         try:
-            t = self._selected_task.pop()
-        except IndexError:
-            return
-
-        try:
-            self.scheduler_downloader_queue.put_nowait(t)
+            while True:
+                all_task.append(self.task_to_downloader.get_nowait())
         except:
-            traceback.print_exc()
-            self._selected_task.append(t)
-            # TODO log it
+            pass
 
-    def load_task_from_queue(self):
+        # 遍历队列
+        for each_task in all_task:
+            if time.time() - each_task['scheduler']['exetime'] > 0:
+                print('[select_task]Select tid:{}'.format(each_task['tid']))
+                self.selected_task.put(each_task)
+            else:
+                # 未到执行时间的，重新翻入队列中
+                self.task_to_downloader.put(each_task)
+        time.sleep(SELECTOR_SLEEP)
+
+    # --------------------------------------------------------
+
+    # ---------------------Thread async queue-----------------
+    async def load_task(self):
         """load item from parser-scheduler queue in a loop,append it to self._task_from_parser"""
         try:
-            self._task_from_parser.append(
-                self._get_item_from_parser_scheduler_queue()
+            t = await self.parser_scheduler_queue.get()
+        except AsyncQueueEmpty:
+            print('[load_task]Redis Queue is empty')
+            await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+            self.loop.create_task(
+                self.load_task()
             )
-        except:
+            return
+        except Exception:
             traceback.print_exc()
+            await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+            self.loop.create_task(
+                self.load_task()
+            )
+            return
 
-    def select_and_push_task(self):
-        """select the task which in up to exetime push task to scheduler-downloader queue in a loop"""
-        self._select_task()
-        self._push_task_to_scheduler_parser_queue()
+        done = False
+        while not done:
+            try:
+                self.task_from_parser.put_nowait(t)
+                done = True
+            except QueueFull:
+                print('[load_task]inner queue is full.')
+            except Exception:
+                traceback.print_exc()
+                await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
 
+        print('[load_task]Load item')
+        self.loop.create_task(
+            self.load_task()
+        )
+
+    async def push_task(self):
+        """ push task to scheduler-downloader queue in a loop"""
+
+        try:
+            t = self.selected_task.get_nowait()
+        except QueueEmpty:
+            print('[push_task]inner queue is empty.')
+            await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+            self.loop.create_task(
+                self.push_task()
+            )
+            return
+        except Exception:
+            traceback.print_exc()
+            await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+            self.loop.create_task(
+                self.push_task()
+            )
+            return
+
+        done = False
+        while not done:
+            try:
+                await self.scheduler_downloader_queue.put(t)
+                done = True
+            except AsyncQueueFull:
+                traceback.print_exc()
+                print('[push_task]Redis queue is full')
+                await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+            except Exception as e:
+                traceback.print_exc()
+                await asyncio.sleep(LOAD_QUEUE_SLEEP, loop=self.loop)
+
+        print('[push_task]Push item')
+        self.loop.create_task(
+            self.push_task()
+        )
+
+    # ---------------------------------------------------------
+
+    # ---------------------Thread scheduler main-----------------
     def _make_task_from_request(self, request):
         """make the task from request and append it to self._task_to_downloader"""
-        self._task_to_downloader.append(
+        self.task_to_downloader.put(
             self.tasker.make(request)
         )
 
-    def _run_ins_func(self, func, items=None):
+    def _run_ins_func(self, spider_name, method_name, item=None):
         """run the spider_ins boned method to return a task,and append it to self._task"""
-        if items:
-            func_return_item = func(items=items)
+        try:
+            spider_ins = self.spider_module_handle.spider_instantiation[spider_name]
+        except IndexError:
+            print('No this Spider or had not instance')
+
+        method = spider_ins.__getattribute__(method_name)
+        if item:
+            func_return_item = method(item=item)
         else:
-            func_return_item = func()
+            func_return_item = method()
 
         if isinstance(func_return_item, dict):
             self._make_task_from_request(func_return_item)
@@ -138,75 +200,92 @@ class Scheduler(object):
     def make_tasks(self):
         """get old task from self._task_from_parser and run ins func to  make new task to append it to self._task"""
         try:
-            task = self._task_from_parser.pop()
-        except IndexError:
+            task = self.task_from_parser.get(timeout=3)
+        except QueueEmpty:
+            print('[make_tasks][QueueEmpty]')
             return
-        callback = task['callback']
 
-        fetcher_func = callback.get('fetcher', None)
-        item = task['parser']['item']
-        # TODO 判断result_pipiline_func是否是可迭代的（可以是list）（在parser里面判断）
-        if fetcher_func:
-            try:
-                # 这里重新生成一个task，如果用户需要保存上一个task的数据（例如meta）必须自己手动处理
-                self._run_ins_func(fetcher_func, item)
-            except:
-                # TODO 这里捕获所有用户编写的spider里面的错误
-                traceback.print_exc()
+        callback = task['callback']
+        spider_name = task['spider_name']
+
+        for callback_method_name in callback:
+            fetcher_method_name = callback_method_name.get('fetcher', None)
+            item = task['parser']['item']
+
+            # TODO 判断result_pipiline_func是否是可迭代的（可以是list）（在parser里面判断）
+            if fetcher_method_name:
+                if not isinstance(fetcher_method_name, list):
+                    fetcher_method_name = [fetcher_method_name]
+                for each_fecher_method_name in fetcher_method_name:
+                    try:
+                        # 这里重新生成一个task，如果用户需要保存上一个task的数据（例如meta）必须自己手动处理
+                        print('[make_tasks] {}.{} making task'.format(spider_name, each_fecher_method_name))
+                        self._run_ins_func(
+                            spider_name,
+                            each_fecher_method_name,
+                            item
+                        )
+                    except:
+                        # TODO 这里捕获所有用户编写的spider里面的错误
+                        traceback.print_exc()
+                        # TODO 这里去重
 
     def make_task_from_start(self):
-        """run spdier_ins start method when a new spider start"""
-        for spider_ins in self.SPIDERS:
-            if spider_ins['status'] == READY_TO_START:
+        """run spider_ins start method when a new spider start"""
+        for spider_name, value in self.loaded_spider.items():
+            if value['status'] == READY_TO_START:
                 # start the spider's start method
-                self._run_ins_func(spider_ins['spider'].start)
-                spider_ins['status'] = STARTED
+                try:
+                    print('[make_task_from_start]starting {}'.format(spider_name))
+                    self._run_ins_func(
+                        spider_name,
+                        'start'
+                    )
+                    value['status'] = STARTED
+                except:
+                    # TODO 这里捕获所有用户编写的spider里面的错误
+                    traceback.print_exc()
 
-    def _stop_spider(self, spider_name):
-        """stop the spider and persist the task"""
-        if spider_name in self.spider_started and spider_name not in self.spider_stoped:
-            self.spider_stoped.add(spider_name)
-            self.spider_started.remove(spider_name)
+    # ----------------------------------------------------------
 
-    def _start_spider(self, spider_name):
-        """stop the spider and persist the task"""
-        if spider_name not in self.spider_started:
-            self.spider_stoped.add(spider_name)
-            if spider_name in self.spider_stoped:
-                self.spider_started.remove(spider_name)
+    # def _stop_spider(self, spider_name):
+    #     """stop the spider and persist the task"""
+    #     if spider_name in self.spider_started and spider_name not in self.spider_stoped:
+    #         self.spider_stoped.add(spider_name)
+    #         self.spider_started.remove(spider_name)
+    #
+    # def _start_spider(self, spider_name):
+    #     """stop the spider and persist the task"""
+    #     if spider_name not in self.spider_started:
+    #         self.spider_stoped.add(spider_name)
+    #         if spider_name in self.spider_stoped:
+    #             self.spider_started.remove(spider_name)
+    #
+    # def handle(self):
+    #     """accept some command to start or stop the spider"""
+    #     pass
 
-    def handle(self):
-        """accept some command to start or stop the spider"""
-        pass
+    def run_async_queue(self):
+        self.loop.create_task(self.load_task())
+        self.loop.create_task(self.push_task())
+        # self.loop.run_forever()
+
+    def run_task_maker(self):
+        while True:
+            self.make_task_from_start()
+            self.make_tasks()
+
+    def run_selector(self):
+        while True:
+            self.select_task()
 
     def run(self):
-        pass
+        t1 = threading.Thread(target=self.run_async_queue)
+        t2 = threading.Thread(target=self.run_task_maker)
+        t3 = threading.Thread(target=self.run_selector)
 
-    def _loop(self):
-        __c__ = 0
-        while (not self.parser_scheduler_queue.empty()
-               and not self.scheduler_downloader_queue.full()
-               and __c__ < self._count_pre_loop
-               ):
-            try:
-                # 接收控制信息->读取spider->实例化spider->运行新的spider的start方法->
-                # 从队列里读取parser发过来的task->运行函数并经过request生成新的task->等待执行时间到，将task放入队列
-                self.handle()
-                self.load_spider()
-                self.instantiate_spider()
-                self.make_task_from_start()
-                self.load_task_from_queue()
-                self.make_tasks()
-                self.select_and_push_task()
-            except KeyboardInterrupt:
-                pass
+        t1.start()
+        t2.start()
+        t3.start()
 
-
-if __name__ == '__main__':
-    tasker = Tasker()
-    # spider = MySpider()
-    # dumped = tasker.dump_task(tasker.make(spider.start())['callback'])
-    # print(dumped)
-
-    # a=tasker.load_task(b'\x80\x03]q\x00(}q\x01(X\x06\x00\x00\x00parserq\x02cbuiltins\ngetattr\nq\x03ccatty.demo.spider\nMySpider\nq\x04)\x81q\x05X\x13\x00\x00\x00parser_content_pageq\x06\x86q\x07Rq\x08X\x07\x00\x00\x00fetcherq\th\x03h\x05X\x0b\x00\x00\x00get_contentq\n\x86q\x0bRq\x0cu}q\r(h\x02h\x03h\x05X\x10\x00\x00\x00parser_list_pageq\x0e\x86q\x0fRq\x10h\th\x03h\x05X\x08\x00\x00\x00get_listq\x11\x86q\x12Rq\x13X\x0f\x00\x00\x00result_pipelineq\x14h\x03h\x05X\t\x00\x00\x00save_listq\x15\x86q\x16Rq\x17ue.')
-    # print(a[0]['parser']('response'))
+        self.loop.run_forever()

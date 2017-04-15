4a122f5a773f494c2c02629e26049228d12628c2
vincent <wax8280@163.com>
2017-04-15 09:37:04 +0800

    add count

---
+++ catty/libs/count.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
+# Author: Vincent<vincent8280@outlook.com>
+#         http://blog.vincentzhong.cn
+# Created on 2017/4/10 19:05
+import time
+import asyncio
+from collections import deque
+
+
+class Counter:
+    def __init__(self, loop):
+        self.loop = loop
+        self.interval = 60
+        self.value_d = {}
+        self.cache_value = {}
+        self.max_size = 1440
+
+    def add_success(self, name):
+        value = self.value_d.setdefault(name + '_success', 0)
+        value = self.value_d.setdefault(name + '_fail', 0)
+        self.value_d.update({name + '_success': value + 1})
+
+    def add_fail(self, name):
+        value = self.value_d.setdefault(name + '_fail', 0)
+        value = self.value_d.setdefault(name + '_success', 0)
+        self.value_d.update({name + '_fail': value + 1})
+
+    async def update(self):
+        for name, count in self.value_d.items():
+            q = self.cache_value.setdefault(name, deque(maxlen=self.max_size))
+            q.append(count)
+        self.value_d.clear()
+        await asyncio.sleep(self.interval, loop=self.loop)
+        self.loop.create_task(self.update())
+
+    def count_all(self):
+
+        result = {}
+        for name, q in self.cache_value.items():
+            if '_success' in name:
+                result.update(
+                    {name: (int(sum(q) / len(q)), int(sum(q) / len(q) * 5), int(sum(q) / len(q) * 60), int(sum(q)))})
+            elif '_fail' in name:
+                result.update(
+                    {name: (int(sum(q) / len(q)), int(sum(q) / len(q) * 5), int(sum(q) / len(q) * 60), int(sum(q)))})
+        return result

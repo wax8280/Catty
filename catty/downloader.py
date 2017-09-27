b1492bfe4b09a2f00d0e287fa94bb289c63ae345
wax8280 <wax8280@163.com>
2017-09-27 22:58:27 +0800

    change the method "aiohttp.requests" to "aiohttp.ClientSession"

--- catty/downloader.py
+++ catty/downloader.py
@@ -10,7 +10,6 @@ import traceback
 from asyncio import BaseEventLoop
 
 import aiohttp
-from aiohttp import BaseConnector
 
 import catty.config
 from catty.message_queue import AsyncRedisPriorityQueue, get_task, push_task
@@ -24,38 +23,55 @@ class DownLoader:
                  scheduler_downloader_queue: AsyncRedisPriorityQueue,
                  downloader_parser_queue: AsyncRedisPriorityQueue,
                  loop: BaseEventLoop,
-                 conn_limit: int,
-                 limit_per_host: int,
-                 force_close: bool):
+                 conn_limit: int):
         """
         :param scheduler_downloader_queue:The redis queue
         :param downloader_parser_queue:The redis queue
         :param loop:EventLoop
         :param conn_limit:Limit of The total number for simultaneous connections.
-        :param limit_per_host:The limit for simultaneous connections to the same endpoint(host, port, is_ssl).
+        # :param limit_per_host:The limit for simultaneous connections to the same endpoint(host, port, is_ssl).
         """
         self.scheduler_downloader_queue = scheduler_downloader_queue
         self.downloader_parser_queue = downloader_parser_queue
 
         self.loop = loop
         self.conn_limit = conn_limit
-        self.aio_conn = aiohttp.TCPConnector(limit=conn_limit,
-                                             loop=self.loop,
-                                             verify_ssl=False,
-                                             # limit_per_host=limit_per_host,
-                                             # deserted in aiohttp lasted version
-                                             force_close=force_close)
 
         # using in conn_limit
         self.count = 0
         self.logger = Log('Downloader')
 
-    async def _request(self, aio_request: Request, loop: BaseEventLoop, connector: BaseConnector) -> Response:
+    async def _request(self, aio_request: Request, loop: BaseEventLoop) -> Response:
         """The real request.It return the Response obj with status 99999 as fail"""
         t_ = time.time()
         self.logger.log_it("Downloading url:{} data:{}".format(aio_request.url, aio_request.data))
         try:
-            async with aiohttp.request(**aio_request.dump_request(), loop=loop, connector=connector) as client:
+            async with aiohttp.ClientSession(loop=loop) as session:
+                if aio_request.method == 'GET':
+                    async with session.get(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'POST':
+                    async with session.post(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'PUT':
+                    async with session.put(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'DELETE':
+                    async with session.delete(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'HEAD':
+                    async with session.head(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'OPTIONS':
+                    async with session.options(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                elif aio_request.method == 'PATCH':
+                    async with session.path(**aio_request.dump_request()) as client:
+                        body = await client.read()
+                else:
+                    self.logger.log_it("Not a vaild method.Request:{}".format(aio_request), level='INFO')
+                    return Response(status=-1, body=str("Not a vaild method.Request:{}".format(aio_request)), )
+
                 response = Response(
                     # TODO text accept encoding param to encode the body
                     # text= await client.text(),
@@ -66,13 +82,13 @@ class DownLoader:
                     charset=client.charset,
                     content_type=client.content_type,
                     # history= client.history,
-                    body=await client.read(),
+                    body=body,
                     use_time=time.time() - t_,
                     url=client.url,
                 )
-                client.close()
+
         except Exception as e:
-            self.logger.log_it("Fail to download url:{} data:{}\nErrInfo:{}".format(aio_request.url, aio_request.data,
+            self.logger.log_it("Fail to download url:{} data:{} ErrInfo:{}".format(aio_request.url, aio_request.data,
                                                                                     traceback.format_exc()))
             response = Response(status=99999, body=str(e), )
 
@@ -95,11 +111,14 @@ class DownLoader:
 
     async def request(self, aio_request: Request, task: dict):
         """request,update the task and put it in the queue"""
-        response = await self._request(aio_request, self.loop, self.aio_conn)
+        response = await self._request(aio_request, self.loop)
         # TODO:99999 means catch exception during request(or we should uniform the status code and write a doc)
         if response['status'] != 99999:
             # success
             await self.success_callback(task, response)
+        elif response['status'] == -1:
+            # -1 means ignore this status
+            pass
         else:
             # fail
             await self.fail_callback(task, aio_request)
